67b23bd755b1f2322fa47274e61760b2d422e4fa
Andrea Torres <andrea@air-de-andrea.mynet>
2020-04-16 17:19:01 +0100

    Lambdas

--- funciones1nivel.py
+++ funciones1nivel.py
@@ -5,6 +5,9 @@ def normal (x):
 def cuadrado (y):
     return y*y
 
+def cubo (x):
+    return x**3
+
 def sumaTodos (limitTo, f):
     resultado = 0
     for i in range (limitTo+1):
@@ -12,5 +15,7 @@ def sumaTodos (limitTo, f):
     
     return resultado
 
-print (sumaTodos(100, normal))
-print (sumaTodos(3, cuadrado)) 
+if __name__ =="__main__": 
+
+    print (sumaTodos(100, normal))
+    print (sumaTodos(3, cuadrado)) 
