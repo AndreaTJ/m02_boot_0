c17a07b2eb4d181d360515b7ace1597297fc7edf
Andrea Torres <andrea@192.168.1.101>
2020-04-17 18:57:37 +0100

    Primera version de perro

---
+++ perro.py
@@ -0,0 +1,7 @@
+class Perro (): #Nombre de la clase, siempre en mayúsculas.
+    #Toda clase, por necesidad, tiene la funcion _init_(FUNCIÓN CONSTRUCTORA) 
+    def _init_(self, nombre, edad, peso):
+        self.nombre = nombre
+        self.edad = edad
+        self.peso = peso 
+        
